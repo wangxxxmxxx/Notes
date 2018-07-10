d71eb41ec8b17930d499c506b8fbfb1b51f8d3ce
wangxueming <29136104+eussi@users.noreply.github.com>
2018-07-11 00:16:53 +0800

    py set

--- note_python/07-list,tuple,string,dict.py
+++ note_python/07-list,tuple,string,dict.py
@@ -247,3 +247,137 @@ for key in info:
 
 for k, v in info.items():             # 会先把dict转成list,数据里大时莫用
     print(k, v)
+#######################################################################################
+# 菜单应用
+print("菜单应用：")
+data = {
+    '北京': {
+        "昌平": {
+            "沙河": ["oldboy", "test"],
+            "天通苑": ["链家地产", "我爱我家"]
+        },
+        "朝阳": {
+            "望京": ["奔驰","陌陌"],
+            "国贸": {"CICC","HP"},
+            "东直门": {"Advent", "飞信"},
+        },
+        "海淀": {},
+    },
+    '山东': {
+        "德州": {},
+        "青岛": {},
+        "济南": {}
+    },
+    '广东':{
+        "东莞": {},
+        "常熟": {},
+        "佛山": {},
+    },
+}
+# exit_flag = False
+#
+# while not exit_flag:
+#     for i in data:
+#         print(i)
+#     choice = input("选择进入1>>:")
+#     if choice in data:
+#         while not exit_flag:
+#             for i2 in data[choice]:
+#                 print("\t",i2)
+#             choice2 = input("选择进入2>>:")
+#             if choice2 in data[choice]:
+#                 while not exit_flag:
+#                     for i3 in data[choice][choice2]:
+#                         print("\t\t", i3)
+#                     choice3 = input("选择进入3>>:")
+#                     if choice3 in data[choice][choice2]:
+#                         while not exit_flag:
+#                             for i4 in data[choice][choice2][choice3]:
+#                                 print("\t\t",i4)
+#                             choice4 = input("最后一层，按b返回>>:")
+#                             if choice4 == "b":
+#                                 break;
+#                             elif choice4 == "q":
+#                                 exit_flag = True
+#                     if choice3 == "b":
+#                         break
+#                     elif choice3 == "q":
+#                         exit_flag = True
+#             if choice2 == "b":
+#                 break
+#             elif choice2 == "q":
+#                 exit_flag = True
+###################################################################################################################
+# 菜单加强版 思路很棒
+# exit_flag = False
+# current_layer = data
+# layers = [data]
+# while not exit_flag:
+#     for k in current_layer:
+#         print(k)
+#     choice = input(">>:")
+#     if choice == "b":
+#         current_layer = layers[-1]
+#         # print("change to laster", current_layer)
+#         layers.pop()
+#     elif choice not in current_layer:
+#         continue
+#     else:
+#         layers.append(current_layer)
+#         current_layer = current_layer[choice]
+################################################################################################
+# 集合，无序的
+print("集合：")
+s = set([3, 5, 9, 10])    # 创建一个数值集合
+t = set([5, 66, 77])
+print(s)
+print(t)
+str_set = set("Hello")    # 创建一个唯一字符的集合,天然去重
+print(str_set)
+a = t | s                 # t 和 s的并集
+print(a)
+b = t & s                 # t 和 s的交集
+print(b)
+c = t - s                 # 求差集（项在t中，但不在s中）
+print(c)
+d = t ^ s                 # 对称差集（项在t或s中，但不会同时出现在二者中）
+print(d)
+# 基本操作：
+t.add('x')                # 添加一项
+print(t)
+s.update([10, 37, 42])    # 在s中添加多项
+print(s)
+print("删除：")
+t.remove('x')             # 使用remove()可以删除一项,不存在会报错 Remove an element from a set; it must be a member.
+                          # If the element is not a member, raise a KeyError.
+t.discard(1)              # Remove an element from a set if it is a member. If the element is not a member, do nothing.
+print(t)
+t.pop()
+print(t)
+del t[0]
+print(len(s))             # set的长度
+print('x' in s )          # 测试x是否是s的成员
+print('x' not in s)       # 测试x是否不是s的成员
+print(s.issubset(t))
+print(s <= t)             # 测试是否s中的每一个元素都在t中
+
+print(s.issuperset(t))
+print(s >= t)             # 测试是否t中的每一个元素都在s中
+
+print(s.union(t))
+print(s | t)              # 返回一个新的set包含s和t中的每一个元素
+
+print(s.intersection(t))
+print(s & t)              # 返回一个新的set包含s和t中的公共元素
+
+print(s.difference(t))
+print(s - t)              # 返回一个新的set包含s中有但是t中没有的元素
+
+print(s.symmetric_difference(t))
+print(s ^ t)              # 返回一个新的set包含s和t中不重复的元素
+
+print(s.copy())           # 返回set “s”的一个浅复制
+
+s = set([1, 2, 3])
+t = set([4, 5, 6])
+print(s.isdisjoint(t))   # 如果连个集合不存在交集返回true
