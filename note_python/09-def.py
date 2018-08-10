1b08e5c694ff7b9752018d9fe6d0c44e9ae6addf
wangxueming <29136104+eussi@users.noreply.github.com>
2018-08-10 21:35:11 +0800

    py def

--- note_python/09-def.py
+++ note_python/09-def.py
@@ -1,4 +1,5 @@
 # wangxueming
+import time
 print("函数：")
 '''
 特性:
@@ -6,7 +7,78 @@ print("函数：")
 2、使程序变的可扩展
 3、使程序变得易维护
 '''
+
+
 # 函数名
-def sayhi():
+def say_hi():
     print("Hello")
-sayhi()  # 调用函数
+
+
+returnSayhi = say_hi()    # 调用函数
+print(returnSayhi)       # 注意返回值为None
+
+
+def logger():                                 # 函数书写规范，定义前后要空两行
+    time_format = '%Y-%m-%d %X'
+    time_current = time.strftime(time_format)
+    with open('TestFileOper2','a+') as f:
+        f.write('%s end action\n' % time_current)
+
+
+def test1():
+    print('in the test1')
+    logger()
+
+
+def test2():
+    print('in the test2')
+    logger()
+
+
+def test3():
+    print('in the test3')
+    logger()
+
+
+test1()
+test2()
+test3()
+
+#############################################################################################
+print("函数返回值：")
+
+
+def test4():
+    pass
+
+
+def test5():
+    return 0
+
+
+def test6():
+    return 0, 'hello', ['a', 'b', 'c'], {'name': 'alex'}
+
+
+x = test4()
+y = test5()
+z = test6()
+print(x)        # 返回值数=0，返回None
+print(y)        # 返回值数=1，返回Object
+print(z)        # 返回值数>1，返回tuple
+
+##############################################################################
+print("参数：")
+
+
+def test(x, y, z):
+    print(x)
+    print(y)
+    print(z)
+
+test(y=2, x=1)         #与形参顺序无关
+test(1,2)             #与形参一一对应
+test(x=2, 3)
+test(3, z=2, y=6)
+
+
