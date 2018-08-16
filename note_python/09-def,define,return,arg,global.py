0b3fa6edfebf45e845b42cff380b1bbb3b6beb95
wangxueming <29136104+eussi@users.noreply.github.com>
2018-08-16 23:34:04 +0800

    py def global

--- note_python/09-def,define,return,arg,global.py
+++ note_python/09-def,define,return,arg,global.py
@@ -143,11 +143,38 @@ def test4(name, age=18, *args, **kwargs):
     print(age)
     print(args)
     print(kwargs)
-    logger("TEST4")
+    logger1("TEST4")
 
 
-def logger(source):
+def logger1(source):              # 注意方法调用方法，方法的定义可以在调用之后，但是实际调用方法时要定义在调用前
     print("from %s" % source)
 
 
 test4('alex', age=34, sex='m', hobby='tesla')
+#############################################################################################################
+print("局部全局变量：")
+school = "DL"
+
+
+def change_name(name1):
+    global school          # 加上此句代表要处理的是外部的全局变量
+    school = "dl"          # 外面不定义该变量也可以，外部即可访问，但是这种加global的方式不建议用，逻辑复杂且混乱
+    print("before change", name, school)
+    name1 = "wxm"               # 这个函数就是这个变量的作用域
+    print(name1)
+
+
+name = "WXM"
+change_name(name)
+print(school)
+
+names = ["Jim", "Jack", "Rain"]
+
+
+def change_name():                     # 传递引用可以改值
+    names[0] = "Correct"
+    print("inside func", names)
+
+
+change_name()
+print(names)
